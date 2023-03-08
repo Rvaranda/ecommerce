59738bb2869c80ba9114ff3d0f094b2688bbd620
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-03-08 13:57:12 -0300

    Created Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+function Banner() {
+  return (
+    <section className="banner">
+        <h2>Bem-vindo à Minha Loja Online</h2>
+        <p>Encontre tudo o que precisa em um só lugar</p>
+        <button class="btn">Compre agora</button>
+    </section>
+  );
+}
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from '../components/Banner';
 import Product from '../components/Product';
 
 function Home() {
   return (
     <main>
+        <Banner />
         <section className="mostSelled">
             <h3>Nossos produtos mais vendidos</h3>
             <div className="products">
