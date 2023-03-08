46fdc0eb6676af6836454cf3dd14a789865fda33
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-03-08 13:27:29 -0300

    Created page header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+
+function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <h1>Minha Loja Online</h1>
+            </div>
+            <Nav />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Início</li>
+                <li>Produtos</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
