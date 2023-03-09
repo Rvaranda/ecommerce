51a8cfc2af4dd00f2a42978a190b5e7009407439
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-03-09 16:05:07 -0300

    Created shop cart button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Nav() {
     return (
-        <nav>
+        <nav style={{ display: 'flex', alignItems: 'center' }}>
             <ul>
                 <li><Link to='/'>Início</Link></li>
                 <li><Link to='/products'>Produtos</Link></li>
                 <li><Link to='/'>Sobre</Link></li>
                 <li><Link to='/'>Contato</Link></li>
             </ul>
+            <Link to='/shopcart' className='shoppingCart'>
+                <FaShoppingCart />
+                Carrinho
+            </Link>
         </nav>
     );
 }
