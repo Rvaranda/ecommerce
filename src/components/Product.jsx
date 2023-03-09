8750ca7bfd292c1388e997d26bd7dc81046772c2
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-03-09 15:40:19 -0300

    Image on product card is now a link

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 function Product({ data }) {
   return (
     <div className='product'>
-      <img src='https://via.placeholder.com/300x300' alt='Produto 1' />
+      <Link to={`/products/${data.id}`}>
+        <img src='https://via.placeholder.com/300x300' alt='Produto 1' />
+      </Link>
       <h4>{data.name}</h4>
       <p>{`R$ ${data.price}`}</p>
       <Link to={`/products/${data.id}`} className='btn'>Comprar</Link>
